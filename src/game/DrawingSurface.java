f5d79dd7ab96141e5f7af204520be4406816e0c1
Nir Reiter <70039366+Nir-Reiter@users.noreply.github.com>
2021-05-14 09:49:15 -0700

    javadocs

--- src/game/DrawingSurface.java
+++ src/game/DrawingSurface.java
@@ -1,19 +1,13 @@
 package game;
 import java.awt.geom.Point2D;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 import g4p_controls.GAlign;
 import g4p_controls.GButton;
@@ -27,6 +21,11 @@ import processing.core.PImage;
 import sprite.Player;
 import sprite.Sprite;
 
+/**
+ * Draws and handles game logic
+ * @author Timothy Li
+ *
+ */
 public class DrawingSurface extends PApplet {
 
 	public static final int WIDTH = 800;
@@ -52,6 +51,10 @@ public class DrawingSurface extends PApplet {
 	public DatabaseReference room;
 	public DatabaseReference thisPlayerRef;
 	
+	/**
+	 * Create a new DrawingSurface which uses a room in the database
+	 * @param room The DatabaseReference for the room the player is in
+	 */
 	public DrawingSurface(DatabaseReference room) {
 		keysHeld = new ArrayList<Integer>();
 		
@@ -107,6 +110,9 @@ public class DrawingSurface extends PApplet {
 		});
 	}
 	
+	/**
+	 * Updates all players and organisms. Called every frame.
+	 */
 	public void update() {
 		thisPlayer.update(this);
 		if (thisPlayer.hasChanged())
@@ -133,14 +139,15 @@ public class DrawingSurface extends PApplet {
 		fill(150, 100, 0);
 		rect(0, 0, gameAreaWidth, gameAreaHeight); // map background
 		
-		thisPlayer.draw(this);
 		for (Sprite s : obstacles)
 			s.draw(this);
+		
+		for(Organism o : organisms)
+			o.draw(this);
+		
 		for (Player p : players.values())
 			p.draw(this);
-		for(Organism o : organisms) {
-			o.draw(this);
-		}
+		thisPlayer.draw(this);
 		
 		popMatrix();
 		
@@ -151,6 +158,12 @@ public class DrawingSurface extends PApplet {
 		text(text, 10, 20);
 	}
 	
+	/**
+	 * Get the x, y position from a point on the screen to the corresponding point in the game field
+	 * @param x X position on the screen
+	 * @param y Y position on the screen
+	 * @return A Point2D.Double containing the game field coordinates
+	 */
 	public Point2D.Double windowToGameField(double x, double y) {
 		return new Point2D.Double(x + thisPlayer.getX() - WIDTH/2, y + thisPlayer.getY() - HEIGHT/2);
 	}
@@ -164,10 +177,17 @@ public class DrawingSurface extends PApplet {
 		keysHeld.remove(new Integer(keyCode));
 	}
 	
+	/**
+	 * @param code The key code to check
+	 * @return true if the key is held, false otherwise
+	 */
 	public boolean isKeyHeld(Integer code) {
 		return keysHeld.contains(code);
 	}
 	
+	/**
+	 * @return -1 if input is to the left, 1 if input is to the right, 0 if no X-axis input
+	 */
 	public int getXAxisInput() {
 		if (isKeyHeld(RIGHT))
 			return 1;
@@ -176,6 +196,9 @@ public class DrawingSurface extends PApplet {
 		return 0;
 	}
 	
+	/**
+	 * @return -1 if input is down, 1 if input is up, 0 if no Y-axis input
+	 */
 	public int getYAxisInput() {
 		if (isKeyHeld(UP))
 			return 1;
@@ -208,6 +231,11 @@ public class DrawingSurface extends PApplet {
 		}
 	}
 
+	/**
+	 * Deals with event on G4P buttons. Called internally.
+	 * @param button The G4P button being used
+	 * @param event The event of the button
+	 */
 	public void handleButtonEvents(GButton button, GEvent event) {
 		if (event == GEvent.CLICKED) {
 			for (int i = 0; i < buttons.size(); i++) {
@@ -219,13 +247,26 @@ public class DrawingSurface extends PApplet {
 		}
 	}
 	
+	/**
+	 * Removes an organism from the game
+	 * @param o Organism to remove
+	 * @return Whether the removal was successful (true if the organism was present, false otherwise)
+	 */
 	public boolean remove(Organism o) {
 		return organisms.remove(o);
 	}
 	
+	/**
+	 * Adds an organism to the game
+	 * @param o Organism to add
+	 */
 	public void add(Organism o) {
 		organisms.add(o);
 	}
+	
+	/**
+	 * @return The list of all organisms in the game
+	 */
 	public ArrayList<Organism> getList() {
 		return organisms;
 	}
@@ -242,18 +283,26 @@ public class DrawingSurface extends PApplet {
 		totalDNA += changeInDNA;
 	}
 	
-	
-	
+	/**
+	 * Listens to changes in the players of a room
+	 * @author Nir Reiter
+	 */
 	public class PlayerListener implements ChildEventListener {
 		
 		private ConcurrentLinkedQueue<Runnable> tasks;
 		
+		/**
+		 * Create a new PlayerListener
+		 */
 		public PlayerListener() {
 			tasks = new ConcurrentLinkedQueue<Runnable>();
 			
 			DrawingSurface.this.registerMethod("post", this);
 		}
 		
+		/**
+		 * Called internally to handle different threads
+		 */
 		public void post() {
 			while (!tasks.isEmpty()) {
 				Runnable r = tasks.remove();
@@ -315,9 +364,33 @@ public class DrawingSurface extends PApplet {
 		
 	}
 	
+	/**
+	 * Listens to changes in the organisms in of the room
+	 * @author Nir Reiter
+	 *
+	 */
 	public class OrganismListener implements ChildEventListener {
 		
 		private ConcurrentLinkedQueue<Runnable> tasks;
+		
+		/**
+		 * Creates a new OrganismListener
+		 */
+		public OrganismListener() {
+			tasks = new ConcurrentLinkedQueue<Runnable>();
+			
+			DrawingSurface.this.registerMethod("post", this);
+		}
+		
+		/**
+		 * Called internally to handle different threads
+		 */
+		public void post() {
+			while (!tasks.isEmpty()) {
+				Runnable r = tasks.remove();
+				r.run();
+			}
+		}
 
 		@Override
 		public void onCancelled(DatabaseError arg0) {
